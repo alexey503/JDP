c79d926170acf80afca9be174415a82770e6806d
Alexey Kochenyuk <alexey503@gmail.com>
2021-05-05 00:50:29 +0300

    Fix

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -3,8 +3,7 @@ package main;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication(scanBasePackages={
-        "main.controllers.DefaultController", "model.BlogRepository"})
+@SpringBootApplication
 public class Main {
     public static void main(String[] args) {
         SpringApplication.run(Main.class, args);
